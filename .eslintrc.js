237b1eb2c8497440eca4db93c49eb3d5409f5dad
roylaw <roylaw.w@gmail.com>
2019-11-20 23:19:33 -0500

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const ERROR = 2;
+const WARN = 1;
+const OFF = 0;
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-native', '@typescript-eslint', 'react-hooks'],
+  env: {
+    'react-native/react-native': true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': OFF,
+    'react-hooks/rules-of-hooks': ERROR,
+    'react-hooks/exhaustive-deps': WARN,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
